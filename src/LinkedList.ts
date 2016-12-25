b80f5157515c0c2031023fc6921041c235bfd57f
Luca Brognara <lucabro_2000@yahoo.it>
2016-12-25 19:26:52 +0100

    added remove node feature

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -14,7 +14,7 @@ class LinkedList {
 
     }
 
-    public initList() {
+    public initList():void {
         this.start = null;
         this.end = null;
     }
@@ -23,7 +23,7 @@ class LinkedList {
      *
      * @param data
      */
-    public addElem(data:any) {
+    public addElem(data:any):void {
         this.addElemRight(data);
     }
 
@@ -31,7 +31,7 @@ class LinkedList {
      *
      * @param data
      */
-    public addElemRight(data:any) {
+    public addElemRight(data:any):void {
 
         var new_elem:ListElement = new ListElement(data);
 
@@ -48,7 +48,7 @@ class LinkedList {
      *
      * @param data
      */
-    public addElemLeft(data:any) {
+    public addElemLeft(data:any):void {
 
         var new_elem:ListElement = new ListElement(data);
 
@@ -63,6 +63,104 @@ class LinkedList {
 
     /**
      *
+     * @param elem
+     */
+    public removeElem(elem:ListElement):void {
+
+        if (this.isStart(elem)) {
+            this.removeStart();
+        }
+        else if (this.isEnd(elem)) {
+            this.removeEnd();
+        }
+        else {
+            var left_elem:ListElement = elem.left;
+            var right_elem:ListElement = elem.right;
+
+            left_elem.right = right_elem;
+            right_elem.left = left_elem;
+        }
+
+        elem = null;
+    }
+
+    /**
+     *
+     * @param pos
+     */
+    public removeElemByPos(pos:number):void {
+        if (pos === 0) {
+            this.removeStart();
+            return;
+        }
+
+        var i:number = 0;
+        var current:ListElement = this.start;
+
+        while (current !== null) {
+            if (i === pos) {
+                this.removeElem(current);
+                return;
+            }
+            current = current.right;
+        }
+    }
+
+    /**
+     *
+     * @param data
+     */
+    public removeElemByData(data:any):void {
+        var current:ListElement = this.start;
+
+        while (current !== null) {
+            if (current.data === data) {
+                this.removeElem(current);
+                return;
+            }
+            current = current.right;
+        }
+    }
+
+    /**
+     * Is passed elem the first of the list?
+     *
+     * @param elem
+     * @returns {boolean}
+     */
+    private isStart(elem:ListElement):boolean {
+        return elem === this.start;
+    }
+
+    /**
+     * Is passed elem the last of the list?
+     *
+     * @param elem
+     * @returns {boolean}
+     */
+    private isEnd(elem:ListElement):boolean {
+        return elem === this.start;
+    }
+
+    /**
+     *
+     */
+    private removeStart():void {
+        this.start = this.start.right;
+        this.start.left = null;
+    }
+
+    /**
+     *
+     */
+    private removeEnd():void {
+        this.end = this.end.left;
+        this.end.right = null;
+    }
+
+    /**
+     * There's an empty list?
+     *
      * @param new_elem
      * @returns {boolean}
      */
@@ -75,10 +173,11 @@ class LinkedList {
     }
 
     /**
+     * Put the first elem in the list
      *
      * @param new_elem
      */
-    private startList(new_elem:ListElement) {
+    private startList(new_elem:ListElement):void {
         this.start = new_elem;
         this.end = this.start;
     }
