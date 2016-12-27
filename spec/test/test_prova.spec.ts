454179115931ad599846526727db028da7a4d57b
lucabro <luca.b@logicweb.it>
2016-12-27 17:52:04 +0100

    basic typescript class for jasmine tests

--- spec/test/test_prova.spec.ts
+++ spec/test/test_prova.spec.ts
@@ -2,13 +2,39 @@
 
 import {LinkedList} from "../../src/core/LinkedList";
 
-describe("test_prova", () => {
+//describe("test_prova", () => {
+//
+//    it("uaddafac", () => {
+//
+//        let linked_list:LinkedList = new LinkedList();
+//        linked_list.initList();
+//        let result = linked_list.start;
+//        expect(result).toEqual(null);
+//    });
+//
+//});
 
-    it("uaddafac", () => {
-        let linked_list:LinkedList = new LinkedList();
-        linked_list.initList();
-        let result = linked_list.start;
-        expect(result).toEqual(null);
-    });
+class LinkedListSpec {
 
-});
+    public linked_list:LinkedList;
+
+    public constructor() {
+        this.linked_list = new LinkedList();
+        this.init(this.addElemTest);
+    }
+
+    public init(func):void {
+        describe("test_prova", func);
+    }
+
+    public addElemTest() {
+        it("uaddafac", () => {
+            let linked_list:LinkedList = new LinkedList();
+            linked_list.initList();
+            let result = linked_list.start;
+            expect(result).toEqual(null);
+        });
+    }
+}
+
+var stica:LinkedListSpec = new LinkedListSpec();
