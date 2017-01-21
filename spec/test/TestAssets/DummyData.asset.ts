683c46cd9150c1423c745fdbb5663404defe53c0
lucabro <luca.b@logicweb.it>
2017-01-21 16:15:08 +0100

    sort testing

--- spec/test/TestAssets/DummyData.asset.ts
+++ spec/test/TestAssets/DummyData.asset.ts
@@ -1,4 +1,5 @@
 import {ListElement} from "../../../src/core/ListElement";
+import {LinkedList} from "../../../src/core/LinkedList";
 
 class DummyData {
 
@@ -6,13 +7,20 @@ class DummyData {
     // DUMMY DATA //
     ////////////////
 
+    public static list:LinkedList<ListElement> = new LinkedList<ListElement>();
+
+    // TODO: for now only with int data, wait for the func param i
+    public static initDummyLinkedList():void {
+        DummyData.list.init(ListElement, []);
+    }
+
     public static A:ListElement = new ListElement("data1");
     public static B:ListElement = new ListElement("data2");
     public static C:ListElement = new ListElement("data3");
     public static D:ListElement = new ListElement("data4");
     public static E:ListElement = new ListElement("data5");
 
-    public static initFirstList() {
+    public static initFirstList():void {
         DummyData.A.prev = null; DummyData.A.next = DummyData.B;
         DummyData.B.prev = DummyData.A; DummyData.B.next = DummyData.C;
         DummyData.C.prev = DummyData.B; DummyData.C.next = DummyData.D;
@@ -28,7 +36,7 @@ class DummyData {
     public static D1:ListElement = new ListElement("data4");
     public static E1:ListElement = new ListElement("data5");
 
-    public static initSecondList() {
+    public static initSecondList():void {
         DummyData.A1.prev = null; DummyData.A1.next = DummyData.C1;
         DummyData.C1.prev = DummyData.A1; DummyData.C1.next = DummyData.E1;
         DummyData.E1.prev = DummyData.C1; DummyData.E1.next = null;
